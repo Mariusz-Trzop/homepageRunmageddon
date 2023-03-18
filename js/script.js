e9128bdd0295a10e945adf5ade74f4c803303936
Mariusz-Trzop <125398791+Mariusz-Trzop@users.noreply.github.com>
2023-03-18 19:24:13 +0100

    adding a function to javascript

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-console.log("Hello Friens!");
+{
+    const welcome = () => {
+        console.log("Hello Friens!")
+    };
 
-let changeThemeButtonElement = document.querySelector(".js-changeThemeButton");
-let headerElement = document.querySelector(".js-header");
-let changeDarkElement = document.querySelector(".js-changeDark");
-let buttonRemovePhotoElement = document.querySelector(".js-buttonRemovePhoto");
-let photoElement = document.querySelector(".js-photo");
-let moreImportantElement = document.querySelector(".js-moreImportant");
-let moreImportantOneElement = document.querySelector(".js-moreImportantOne");
-let moreImportantTwoElement = document.querySelector(".js-moreImportantTwo");
-let moreImportantTreeElement = document.querySelector(".js-moreImportantTree");
-let changeTextElement = document.querySelector(".js-changeText");
-let footerElement = document.querySelector(".js-footer");
+    const toggleBackground = () => {
+        const headerElement = document.querySelector(".js-header");
+        const changeDarkElement = document.querySelector(".js-changeDark");
+        const moreImportantElement = document.querySelector(".js-moreImportant");
+        const moreImportantOneElement = document.querySelector(".js-moreImportantOne");
+        const moreImportantTwoElement = document.querySelector(".js-moreImportantTwo");
+        const moreImportantTreeElement = document.querySelector(".js-moreImportantTree");
+        const footerElement = document.querySelector(".js-footer");
+        headerElement.classList.toggle("header__lightTheme");
+        moreImportantElement.classList.toggle("section__moreImportant--lightTheme");
+        moreImportantOneElement.classList.toggle("section__moreImportant--lightTheme");
+        moreImportantTwoElement.classList.toggle("section__moreImportant--lightTheme");
+        moreImportantTreeElement.classList.toggle("section__moreImportant--lightTheme");
+        footerElement.classList.toggle("footer__lightTheme");
+        changeDarkElement.innerText = headerElement.classList.contains("header__lightTheme") ? "ciemny" : "jasny";
+    };
 
-changeThemeButtonElement.addEventListener("click", () => {
-    headerElement.classList.toggle("header__lightTheme");
-    moreImportantElement.classList.toggle("section__moreImportant--lightTheme");
-    moreImportantOneElement.classList.toggle("section__moreImportant--lightTheme");
-    moreImportantTwoElement.classList.toggle("section__moreImportant--lightTheme");
-    moreImportantTreeElement.classList.toggle("section__moreImportant--lightTheme");
-    footerElement.classList.toggle("footer__lightTheme");
-    changeDarkElement.innerText = headerElement.classList.contains("header__lightTheme") ? "ciemny" : "jasny";
-});
+    const toogleDisplayPhoto = () => {
+        const photoElement = document.querySelector(".js-photo");
+        const changeTextElement = document.querySelector(".js-changeText");
+        photoElement.classList.toggle("section__photo--display");
+        changeTextElement.innerText = photoElement.classList.contains("section__photo--display") ? "Pokaż" : "Ukryj";
+    };
 
-buttonRemovePhotoElement.addEventListener("click", () => {
-    photoElement.classList.toggle("section__photo--display");
-    changeTextElement.innerText = photoElement.classList.contains("section__photo--display") ? "Pokaż" : "Ukryj";
-});
+    const init = () => {
+        const changeThemeButtonElement = document.querySelector(".js-changeThemeButton");
+        const buttonRemovePhotoElement = document.querySelector(".js-buttonRemovePhoto");
+        changeThemeButtonElement.addEventListener("click", toggleBackground);
+        buttonRemovePhotoElement.addEventListener("click", toogleDisplayPhoto);
 
+        welcome();
+    };
 
+    init();
 
+}
